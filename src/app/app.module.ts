6903bb6429937842d7d7e1f5b1c8e32b4ab6b43f
Your Name <you@example.com>
2020-12-19 11:23:50 +0100

    :bug: fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -11,8 +15,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+  constructor() {
+    library.add(fas, far);
+  }
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import {Routes} from '@angular/router';
+
+const routes: Routes = [
+  { path: 'calc', component: FirstComponent },
+  { path: 'import', component: SecondComponent },
+  { path: 'export', component: FirstComponent },
+  { path: 'settings', component: SecondComponent },
+];
