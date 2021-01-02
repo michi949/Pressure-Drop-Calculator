6ea0b7e13930d218d2b01e294c1e911fe76e4f88
Your Name <you@example.com>
2021-01-02 22:23:17 +0100

    Added stuff to fix and not to be sad because it doesent work like i want...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { ProjectManagement } from './services/project-management.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ export class AppComponent implements OnInit {
   title = 'Reder Systems Druckverlust';
 
 
-  public constructor(private projectManagement: ProjectManagement) {
+  public constructor(private projectManagement: ProjectManagement,
+                     public router: Router) {
   }
 
   ngOnInit(): void {
