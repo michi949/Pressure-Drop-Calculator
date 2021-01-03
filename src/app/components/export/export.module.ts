83cfa1094f7bab31d74137a1796943b15513eceb
Your Name <you@example.com>
2021-01-03 21:13:25 +0100

    Added Export View

--- src/app/components/export/export.module.ts
+++ src/app/components/export/export.module.ts
@@ -4,14 +4,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ExportPage } from './export.page';
 import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
+import {AppRoutingModule} from '../../app.routes';
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        FontAwesomeModule,
-        CommonModule
-    ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    CommonModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
     declarations: [
         ExportPage
     ],
--- src/app/components/export/export.page.ts
+++ src/app/components/export/export.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import {ProjectManagement} from '../../services/project-management.service';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
     selector: 'app-export',
@@ -7,16 +8,27 @@ import {ProjectManagement} from '../../services/project-management.service';
     styleUrls: ['./export.page.scss'],
   })
   export class ExportPage implements OnInit, OnDestroy  {
+    public pdfName = new FormControl('', [Validators.required]);
+    public excelName = new FormControl('', [Validators.required]);
 
-    public constructor(public projectManagement: ProjectManagement) {
-
-    }
+    public constructor(public projectManagement: ProjectManagement) {}
 
     ngOnInit(): void {
-
+      if (this.projectManagement.currentProject !== null) {
+        const name = this.projectManagement.currentProject.name;
+        this.pdfName.setValue(name);
+        this.excelName.setValue(name);
+      }
     }
     ngOnDestroy(): void {
 
     }
 
+    public exportPDF() {
+      console.log(this.pdfName.value);
+    }
+
+    public exportExcel() {
+      console.log(this.excelName.value);
+    }
   }
